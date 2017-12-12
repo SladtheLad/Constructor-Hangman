c1a6e6559aeefc05d02148945ce5c823da290f54
Nicholas Sladden <nickoliasxii@gmail.com>
2017-12-12 12:57:08 -0600

    hand hurts

---
+++ app.js
@@ -0,0 +1,11 @@
+/* Things to do for HANGMAN GAME:
+* USE hangman-cli-art npm
+* USE inquirer
+
+*/
+var HangmanArt = require('hangman-cli-art');
+var art = new HangmanArt({
+    marginX: 5,
+    marginY: 5
+});
+art.next();
--- app.ts
+++ app.ts
@@ -1,3 +1,16 @@
 /* Things to do for HANGMAN GAME:
 * USE hangman-cli-art npm
-* USE prompt npm
+* USE inquirer
+
+*/
+//This sets up a node package that renders Hangman styled art for the game in the terminal
+const HangmanArt = require("hangman-cli-art");
+
+const art = new HangmanArt({
+  marginX: 5,
+  marginY: 0
+});
+
+//Loads and allows use of Inquirer node package
+const inquirer = require("inquirer");
+
